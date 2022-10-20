083ccb13a4656122e4e81b3c292e31f71bed0e8b
Brandon Rdgz <brandon.rdgz@gmail.com>
2022-10-20 03:10:01 -0500

    Initial state of project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flut_nasa_image_lib
+
+A Flutter Nasa Image Library app.
---
+++ android/app/src/main/kotlin/com/rodua/flut_nasa_image_lib/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rodua.flut_nasa_image_lib
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
